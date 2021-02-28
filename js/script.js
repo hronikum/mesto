486606dcf6bb76e2bfa082adb172d02d658c2d95
Zhostya Kilin <kostya.zhilin@gmail.com>
2021-02-28 23:03:06 +0300

    fix: внести правки ревьюера

--- js/script.js
+++ js/script.js
@@ -3,31 +3,31 @@
 let profileName = document.querySelector('.profile__author-name');
 let profileJob = document.querySelector('.profile__author-description');
 
-let editProfileBtn = document.querySelector('.profile__edit-btn')
-let popupBlock = document.querySelector('.popup')
-let ClosePopupBtn = document.querySelector('.popup__close-btn')
+let editProfileBtn = document.querySelector('.profile__edit-btn');
+let popupBlock = document.querySelector('.popup');
+let closePopupBtn = document.querySelector('.popup__close-btn');
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__form');
 
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector('#nameInput')
-let jobInput = formElement.querySelector('#jobInput')
+let nameInput = formElement.querySelector('#nameInput');
+let jobInput = formElement.querySelector('#jobInput');
 
 
 function showPopup() {  
-    nameInput.value = profileName.textContent
-    jobInput.value = profileJob.textContent
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 
-    popupBlock.classList.add('popup_opened')  
+    popupBlock.classList.add('popup_opened');
 }
 editProfileBtn.addEventListener('click', showPopup); 
 
 function closePopup() {
-    popupBlock.classList.remove('popup_opened')  
+    popupBlock.classList.remove('popup_opened');
 }
 
-ClosePopupBtn.addEventListener('click', closePopup)
+closePopupBtn.addEventListener('click', closePopup);
 
 
 
@@ -37,15 +37,11 @@ ClosePopupBtn.addEventListener('click', closePopup)
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
 
-    // Получите значение полей jobInput и nameInput из свойства value
-    let newNameInput = nameInput.value;
-    let newJobInput = jobInput.value
-
     // Вставьте новые значения с помощью textContent
-    profileName.textContent = newNameInput
-    profileJob.textContent = newJobInput
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
 
-    closePopup()
+    closePopup();
 }
 
 // Прикрепляем обработчик к форме:
