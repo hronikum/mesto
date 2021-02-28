f299f8e7f50c3b842da57c13ebecfa470d953444
Zhostya Kilin <kostya.zhilin@gmail.com>
2021-02-28 15:25:32 +0300

    feat: доделать popup, добавить js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,51 @@
+//
+// Выберите элементы, куда должны быть вставлены значения полей
+let profileName = document.querySelector('.profile__author-name');
+let profileJob = document.querySelector('.profile__author-description');
+
+let editProfileBtn = document.querySelector('.profile__edit-btn')
+let popupBlock = document.querySelector('.popup')
+let ClosePopupBtn = document.querySelector('.popup__close-btn')
+
 // Находим форму в DOM
-let formElement = // Воспользуйтесь методом querySelector()
+let formElement = document.querySelector('.popup__form');
+
 // Находим поля формы в DOM
-let nameInput = // Воспользуйтесь инструментом .querySelector()
-let jobInput = // Воспользуйтесь инструментом .querySelector()
+let nameInput = formElement.querySelector('#nameInput')
+let jobInput = formElement.querySelector('#jobInput')
+
+
+function showPopup() {  
+    nameInput.value = profileName.textContent
+    jobInput.value = profileJob.textContent
+
+    popupBlock.classList.add('popup_opened')  
+}
+editProfileBtn.addEventListener('click', showPopup); 
+
+function closePopup() {
+    popupBlock.classList.remove('popup_opened')  
+}
+
+ClosePopupBtn.addEventListener('click', closePopup)
+
+
+
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
+    evt.preventDefault(); 
 
     // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
+    let newNameInput = nameInput.value;
+    let newJobInput = jobInput.value
 
     // Вставьте новые значения с помощью textContent
+    profileName.textContent = newNameInput
+    profileJob.textContent = newJobInput
+
+    closePopup()
 }
 
 // Прикрепляем обработчик к форме:
