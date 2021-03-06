84f2f8a190a6a5714a2b228aa6d66bdbb3948b0d
Zhostya Kilin <kostya.zhilin@gmail.com>
2021-03-06 18:59:23 +0300

    fix: заменить for-each на map в рендере страницы

--- js/script.js
+++ js/script.js
@@ -1,49 +1,109 @@
-//
-// Выберите элементы, куда должны быть вставлены значения полей
-let profileName = document.querySelector('.profile__author-name');
-let profileJob = document.querySelector('.profile__author-description');
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
 
-let editProfileBtn = document.querySelector('.profile__edit-btn');
-let popupBlock = document.querySelector('.popup');
-let closePopupBtn = document.querySelector('.popup__close-btn');
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');
-
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('#nameInput');
-let jobInput = formElement.querySelector('#jobInput');
-
-
-function showPopup() {  
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
-
-    popupBlock.classList.add('popup_opened');
-}
+// Блок элементов страницы
+const cards = document.querySelector('.cards');
+const profileName = document.querySelector('.profile__author-name');
+const profileJob = document.querySelector('.profile__author-description');
+const editProfileBtn = document.querySelector('.profile__edit-btn');
 editProfileBtn.addEventListener('click', showPopup); 
+const addBtn = document.querySelector('.profile__add-btn');
+addBtn.addEventListener('click', showPopupNewPlace); 
+
+// Блок с popup формы редактирования пользователя
+const popupBlock = document.querySelector('.popup');
+const formElement = document.querySelector('.popup__form');
+formElement.addEventListener('submit', formSubmitHandler); 
+const nameInput = formElement.querySelector('#nameInput');
+const jobInput = formElement.querySelector('#jobInput');
+nameInput.value = profileName.textContent;
+jobInput.value = profileJob.textContent;
+const closePopupBtn = document.querySelector('.popup__close-btn');
+closePopupBtn.addEventListener('click', closePopup);
+
+// Блок с popup формы добавленяия нового места
+const popupNewPlace = document.querySelector('.popupAddNewPlace');
+
+
+function showPopup() {
+  popupBlock.classList.add('popup_opened');
+}
+
+function showPopupNewPlace() {
+  popupNewPlace.classList.add('popup_opended')
+}
+
 
 function closePopup() {
     popupBlock.classList.remove('popup_opened');
 }
 
-closePopupBtn.addEventListener('click', closePopup);
 
-
-
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
 
-    // Вставьте новые значения с помощью textContent
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
 
     closePopup();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler); 
+//  Создаем место
+function createCard(place) {
+  const cardExample = document.querySelector('#cardTemplate').content.cloneNode(true);
+  const cardImage = cardExample.querySelector('.card__image');
+  const cardTitle = cardExample.querySelector('.card__title');
+  const cardLikeBtn = cardExample.querySelector('.card__like-btn');
+  
+  cardImage.src = place.link
+  cardImage.alt = place.name
+  cardTitle.textContent = place.name
+
+  cardImage.addEventListener('click', evt => {
+    const eventTarget = evt.target;
+    showPopupImg(place.name, place.link);
+  });
+  
+  cardLikeBtn.addEventListener('click', evt => {
+    const eventTarget = evt.target;
+    eventTarget.classList.toggle('card__like-btn_active');
+  }); 
+  
+  return cardExample;
+  
+};
+
+function showPopupImg(title, imgLink) {
+  console.log(title, imgLink)
+};
+
+function initialRender() {
+  const result = initialCards.map(createCard);
+  cards.append(...result);
+};
+
+initialRender();
