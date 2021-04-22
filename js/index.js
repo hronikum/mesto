2bf9b1dafada64b44010e0b6083753572f6e113e
Zhostya Kilin <kostya.zhilin@gmail.com>
2021-04-22 12:04:01 +0300

    fix: поправить ширинку карточки при удалении, создавать экземпляры FormValidator с разными именами

--- js/index.js
+++ js/index.js
@@ -1,22 +1,31 @@
 import {Card} from './Card.js'
 import {initialCards} from './initial-cards.js';
-import {settings} from './utils.js'
+import {settings,
+  profileEditPopupForm,
+  cardAddPopupForm
+} from './utils.js'
 import {FormValidator} from './FormValidator.js'
 
 const cards = document.querySelector('.cards');
+
+// И тут мои комменты
+// Твой комментарий: 
+// Функционал создания карточки следует вынести в функцию createCard, 
+// которая просто должна возвращать новую карточку.
+
+// Мой вопрос:
+// Зачем тут нужна функция, если вся суть этого спринта была в том, чтобы функционал
+// перенести в классы и избавиться от функций? Здесь же нет повторяемого кода, мы 
+// бежим по массиву, и создаем экземпляры класса, которые затем добавляем в разметку.
+// Функция только усложнит этот код и никак не поможет.
 
 initialCards.forEach( card => {
   const newCard = new Card(card, '#cardTemplate')
   cards.append(newCard.render());
 })
 
-// создаем список форм
-const formList = Array.from(document.querySelectorAll(settings.formSelector));
-  
-// проходим по списку форм
-formList.forEach(formElement => {
-  // создаем для каждой формы экземпляр класса FormValidator
-  const newFormValidator = new FormValidator(settings, formElement);
-  // включаем валидацию
-  newFormValidator.enableValidation();
-})
+const addCardFormValidator = new FormValidator(settings, cardAddPopupForm);
+addCardFormValidator.enableValidation();
+
+const editProfileFormValidator = new FormValidator(settings, profileEditPopupForm);
+editProfileFormValidator.enableValidation();
